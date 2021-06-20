1555631c3b5ba97dc1f04b79eddddee26a3e6e53
JenifferHerrera <jmm.herrera@yavirac.edu.ec>
2021-06-20 06:39:45 -0500

    consola2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MiPrimerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hola Mundo");
+
+            if (args != null && args.Length > 0)
+            {
+                Console.WriteLine($"Hay" + args.Length + "Argumentos");
+                Console.WriteLine($"Tu nombre es: " + args[0]);
+                Console.WriteLine($"Tu apellido es: " + args[1]);
+            }
+            else
+                Console.WriteLine("No hay Argumentos");
+        }
+    }
+}
